739d159e094aee07c680abdc71b4dd7fa8e9ca97
elcosas <121999809+elcosas@users.noreply.github.com>
2025-01-25 13:38:36 -0800

    Setup basic api endpoint for backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, methods=['POST'])
 
 
-@app.route('/')
-def index():
-    return 'hi :)'
+@app.route('/predict', methods=['POST'])
+def get_housing_prediction():
+    address: str = request.form.get('address')
 
+    # Mock response
+    response = {
+        'address': address,
+        'prices': [(x, x**2) for x in range(2000, 2040)],
+    }
+
+    return jsonify(response)
